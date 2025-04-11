87253118806cb1fb33485533182a45ef1674621e
lupusludens-se <vic.gurov@se.com>
2025-04-10 22:17:10 -0400

    10_apr_25_22h18m close browser for UI is added

--- ui-tests/specs/BasePage.spec.js
+++ ui-tests/specs/BasePage.spec.js
@@ -5,4 +5,5 @@ const { test, expect } = require('@playwright/test');
 test('BasePage test', { tag: '@basePage' }, async ({ page }) => {
   await page.goto(process.env.UI_BASE_URL);
   await expect(page).toHaveTitle(/Zeigo Network/);
+  await page.close();
 });
