7b65f382da5189bf8beb18075705b0303079c811
lupusludens-se <vic.gurov@se.com>
2025-04-23 15:26:57 -0400

    23_apr_25_15h25m run_tests_scheduler.bat is updated

--- run_tests_scheduler.bat
+++ run_tests_scheduler.bat
@@ -6,10 +6,10 @@ set /p DELAY="Enter the delay (in seconds) between batches: "
 
 :loop
 for /L %%i in (1, 1, %BATCH_COUNT%) do (
-    echo Running test batch %%i...    call npx playwright test
+    echo Running test batch %%i...
+    npx playwright test all-tests
     echo Test batch %%i completed.
     if %%i NEQ %BATCH_COUNT% ping -n %DELAY% 127.0.0.1 > nul
-    timeout /t 2 > nul
 )
 echo All batches completed. Waiting before restarting...
 ping -n %DELAY% 127.0.0.1 > nul
